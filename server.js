944fe8ef03d916916d09290b73c1b0a7e1ca5af3
Liam Condon <condonliamt@gmail.com>
2019-04-18 21:05:04 -0700

    test commit

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-
 // Use morgan logger for logging requests
 app.use(logger("dev"));
 
